9432e0241dff1e79462e57653034f7139388c00f
Christian Alexander Vazquez Gonzalez <dev.chrisvaz@gmail.com>
2024-02-23 03:15:47 +0000

    first c file uploaded

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
 /**
-* main function return a text"
-*
-* returns always 0(success)
+* main - function that returns a text
+* 
+* Return:  0 (success)
 */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
